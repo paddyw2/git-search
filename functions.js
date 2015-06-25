c02adc0c4bf583c05604a249c585c3d7e2524953
Patrick <p.withams@gmail.com>
2015-06-25 13:50:22 -0600

    updating js and adding style - formatted js, changed function type to control errors and added basic stylings

--- functions.js
+++ functions.js
@@ -1,42 +1,64 @@
-
 $(document).ready( function () {
 
+// search function
 
+$( "form" ).submit(function() {
     
+    // getting user input from form
     
-    $( "form" ).submit(function() {
+    var username = $('#entry_field').val();
+    var githubAPI = "https://api.github.com/users/" + username;
     
-        
-        var username = $('#entry_field').val();
-  
-        var githubAPI = "https://api.github.com/users/" + username;
-  
-        
-        
-        $.getJSON( githubAPI, function( data ) {
+    // starting ajax function to get json data
+    
+    $.ajax({
+        url: githubAPI,
+        dataType: "json",
+        success: function(data) {
             
-  var name = data.name;
-  var blog = data.blog;
-  var location = data.location;
-  var image = data.avatar_url;
+            // if data is successfully fetched
             
-            if (name) {
-    
-  var div = document.getElementById('output');
-  div.innerHTML = name + '<br>';
-    
-  var div2 = document.getElementById('output2');
-  div2.innerHTML = blog + '<br>';
-
-  var div3 = document.getElementById('output3');
-  div3.innerHTML = "<img src=' " +image+ " ' >";
-            } else {
-                alert('User not found');
-                      };
-    
+            $.getJSON( githubAPI, function( data ) {
+                
+                // target specific data
+                
+                var name = data.name;
+                var blog = data.blog;
+                var location = data.location;
+                var image = data.avatar_url;
+                
+                // if user has all required data, output on page
+                
+                if (name && blog && location && image) {
+                    
+                    $('.welcome').empty();
+                    $('.container').addClass('slideUp');
+                    $('.display').addClass('fadeIn');
+                    $('.menu').addClass('show');
+                    $('#entry_field').val('');
+                    $( "#entry_field" ).blur();
+                    $( "#submit" ).blur();
+                    
+                    var div = document.getElementById('output');
+                    div.innerHTML = '<h1>' + name + '</h1><br>';
+                    var div2 = document.getElementById('output2');
+                    div2.innerHTML = '<a href='+blog+'><p>' + blog + '</p></a><br>';
+                    var div3 = document.getElementById('output3');
+                    div3.innerHTML = "<img src=' " +image+ " ' >";
+                } else {
+                    alert('user does not have enough info!');
+                };  
+            });
+        },
+        
+        // if data is not successfully fetched
+        
+        error: function(data) {
+        alert('User does not exist!');
+        }
+    });  
 });
-        
-        
-    });
-    
+
+// end search function
+
 });
